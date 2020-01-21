84c7c57137cfbc6d72abb5d31bff3d4d9e4cd701
katerynashydlovska <shydlovskayaka@gmail.com>
2020-01-20 19:05:21 -0800

    odd number

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// Challenge -1
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
 // If the function is passed a valid PIN string, return true, else return false.
 
@@ -11,5 +12,41 @@ function validatePIN(pin) {
   }
 }
 
+// shortcut solution
+// function validatePIN(pin) {
+//   return /^(\d{4}|\d{6})$/.test(pin)
+// }
 
-s
+// errow function
+// const validatePIN = pin => /^(\d{4}$|\d{6}$)/.test(pin);
+
+
+// Challenge -2
+// Given an array, find the integer that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+
+function findOdd(A) {
+  let num = {};
+  for (let i = 0; i < A.length; i++) {
+    if (num[A[i]]) {
+      num[A[i]] += 1
+      //  console.log('+1', num);
+    } else {
+      num[A[i]] = 1
+      // console.log('=1', num);
+    }
+  }
+  let oddTimes = Object.keys(num);
+  //  console.log(Object.keys(num))
+  for (j = 0; j < oddTimes.length; j++) {
+    if (num[oddTimes[j]] % 2 !== 0) {
+      return parseInt(oddTimes[j])
+    }
+  }
+}
+
+// function findOdd(A) {
+   // return A.reduce(function (c, v) { return c ^ v; }, 0);
+// }
