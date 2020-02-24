de3173d42f3f420e26830f92426935065c723df6
katerynashydlovska <shydlovskayaka@gmail.com>
2020-02-23 18:03:33 -0800

    codewars

--- app.js
+++ app.js
@@ -1164,5 +1164,24 @@ function remove(value, node){
   return;
 }
 
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([1, 1, 1, 2, 1, 1]) === 2
+// findUniq([0, 0, 0.55, 0, 0]) === 0.55
+
+function findUniq(arr) {
+  let i = 0;
+  if (arr[0] !== arr[1] && arr[1] === arr[2]) {
+    return arr[0];
+  }
+  while (i < arr.length) {
+    if (arr[i] !== arr[i + 1]) {
+      return arr[i + 1];
+    }
+    i++;
+
+  }
+}
+
 
 
