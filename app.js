0a70fe31aa3dd917aafdcb8efe3fc75867d0770f
katerynashydlovska <shydlovskayaka@gmail.com>
2020-02-23 17:38:35 -0800

    code wars one more solution

--- app.js
+++ app.js
@@ -1132,6 +1132,19 @@ function persistence(num) {
   return count;
 }
 
+// function persistence(num) {
+//   let times = 0;
+
+//   num = num.toString();
+
+//   while (num.length > 1) {
+//     times++;
+//     num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+//   }
+
+//   return times;
+// }
+
 
 
 // remove  dublicates from the linked list
