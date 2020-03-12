cfbcd62032807d1d710e9d9eaadf9d3334017a85
katerynashydlovska <shydlovskayaka@gmail.com>
2020-03-11 19:44:40 -0700

    arr slice

--- app.js
+++ app.js
@@ -1523,3 +1523,20 @@ function solution(str) {
   }
   return newStr;
 }
+
+
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order.The array will always include at least 2 items.
+
+// For example:
+
+// twoOldestAges([1, 2, 10, 8]) // should return [8, 10]
+
+function twoOldestAges(ages) {
+  let sortedAge = ages.sort((a, b) => a - b);
+  console.log(sortedAge)
+  return sortedAge.slice(-2);
+}
+
+twoOldestAges(ages);
