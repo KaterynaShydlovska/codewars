e562d8f7699d3841a1967ecc20a53d309c2abb23
katerynashydlovska <shydlovskayaka@gmail.com>
2020-03-19 20:50:15 -0700

    regex

--- regex/app.js
+++ regex/app.js
@@ -49,3 +49,19 @@ function alphanumeric(string) {
   }
   return false;
 }
+
+
+// Challenge 4
+
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a number
+// Valid passwords will only be alphanumeric characters.
+
+function validate(password) {
+  console.log(password)
+  return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$/.test(password);
+}
