0bf40d822ce12e272cb5f5f10249473fa425b5fa
katerynashydlovska <shydlovskayaka@gmail.com>
2020-03-23 13:23:33 -0700

    fibonacci

--- fibonacci/app.js
+++ fibonacci/app.js
@@ -18,4 +18,19 @@ function nthFibo(n) {
 console.log(nthFibo(3));
 
 
+//  fibonacci=0, 1, 1, 2, 3, 5, 8, 13, 21, 34..;
+
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2)
+  }
+}
+
+console.log(fib(6));
+
+
 
