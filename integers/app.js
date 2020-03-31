2d55841b73e929a24dfb94e75faf457da1106d6e
katerynashydlovska <shydlovskayaka@gmail.com>
2020-03-30 17:04:43 -0700

    leetcode int

--- integers/app.js
+++ integers/app.js
@@ -402,3 +402,40 @@ var findNumbers = function (nums) {
   return even;
 
 };
+
+
+
+// Challenge 14
+
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+//   Example 1:
+
+// Input: n = 234
+// Output: 15
+// Explanation:
+// Product of digits = 2 * 3 * 4 = 24
+// Sum of digits = 2 + 3 + 4 = 9
+// Result = 24 - 9 = 15
+// Example 2:
+
+// Input: n = 4421
+// Output: 21
+// Explanation:
+// Product of digits = 4 * 4 * 2 * 1 = 32
+// Sum of digits = 4 + 4 + 2 + 1 = 11
+// Result = 32 - 11 = 21
+
+var subtractProductAndSum = function (n) {
+  let sum = 0;
+  let mult = 1;
+  let newN = n.toString().split('');
+  for (let i = 0; i < newN.length; i++) {
+    sum += parseInt(newN[i]);
+  }
+  console.log(sum)
+  for (let j = 0; j < newN.length; j++) {
+    mult *= parseInt(newN[j]);
+  }
+  console.log(mult)
+  return mult - sum;
+};
