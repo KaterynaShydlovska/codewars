b54beb6b28fa0e5713f4a20a280ac86bc1cf18c2
katerynashydlovska <shydlovskayaka@gmail.com>
2020-03-31 20:59:19 -0700

    fixed last challenge

--- array/app.js
+++ array/app.js
@@ -512,11 +512,12 @@ var sortArrayByParity = function (A) {
 // return [0, 1].
 
 var twoSum = function (nums, target) {
-  let i = 0;
-  let j = i + 1;
-  while (j < nums.length - 1) {
-    if (nums[i] + nums[j] === target) {
-      return [i, j]
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      // console.log(nums[i], nums[j])
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
     }
   }
 };
