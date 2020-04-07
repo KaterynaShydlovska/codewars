db8ec0dfd4ea5951ed4518cc20a2b4d9b75f6529
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-06 21:08:38 -0700

    is happy number

--- integers/app.js
+++ integers/app.js
@@ -525,3 +525,36 @@ function expressionMatter(a, b, c) {
   return Math.max(...arr);
 
 }
+
+// Write an algorithm to determine if a number n is "happy".
+
+// A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1(where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+// Return True if n is a happy number, and False if not.
+
+//   Example:
+
+//   Input: 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+var isHappy = function (n, counter = 0) {
+  let str = n.toString().split('');
+  let sum = 0;
+  if (counter > 20) {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+    sum += str[i] ** 2;
+  }
+  if (sum === 1) {
+    return true;
+  } else {
+    counter++
+    return isHappy(sum, counter);
+  }
+};
