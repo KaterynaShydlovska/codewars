2bedce43c5b79fa873680b06608c57decb54d986
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-06 21:29:19 -0700

    max subArr

--- array/app.js
+++ array/app.js
@@ -686,3 +686,35 @@ function minSubArrayLen(nums, sum) {
 
   return minLen === Infinity ? 0 : minLen;
 }
+
+// Challenge 23
+
+// Given an integer array nums, find the contiguous subarray(containing at least one number) which has the largest sum and return its sum.
+
+//   Example:
+
+// Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+//   Output: 6
+// Explanation: [4, -1, 2, 1] has the largest sum = 6.
+// Follow up:
+
+// If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+var maxSubArray = function (nums) {
+  let i = 0;
+  let maxSum = Math.max(...nums);
+  while (i < nums.length - 1) {
+    tempSum = nums[i];
+    let j = i + 1;
+    while (j < nums.length) {
+      tempSum += nums[j];
+      if (tempSum > maxSum) {
+        maxSum = tempSum;
+      }
+      j++;
+    }
+    i++;
+  }
+  return maxSum;
+
+};
