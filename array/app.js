1173a45d88db3129e9091243466b1eb3b9f63247
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-07 21:02:06 -0700

    Counting Elements

--- array/app.js
+++ array/app.js
@@ -864,3 +864,57 @@ var maxProfit = function (prices) {
   }
   return profits;
 };
+
+
+// Challenge 27
+
+// Counting Elements
+
+// Given an integer array arr, count element x such that x + 1 is also in arr.
+
+// If there're duplicates in arr, count them seperately.
+
+
+
+// Example 1:
+
+// Input: arr = [1, 2, 3]
+// Output: 2
+// Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+//   Example 2:
+
+// Input: arr = [1, 1, 3, 3, 5, 5, 7, 7]
+// Output: 0
+// Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+// Example 3:
+
+// Input: arr = [1, 3, 2, 3, 5, 0]
+// Output: 3
+// Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+//   Example 4:
+
+// Input: arr = [1, 1, 2, 2]
+// Output: 2
+// Explanation: Two 1s are counted cause 2 is in arr.
+
+var countElements = function (arr) {
+  let counter = 0;
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
+      obj[arr[i]] += 1;
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+  for (let j = 0; j < arr.length; j++) {
+    if (obj[arr[j] + 1]) {
+      counter += 1;
+      // obj[arr[j]+1]-=1;
+
+    }
+  }
+  return counter;
+
+};
