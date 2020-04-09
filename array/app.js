7116adb510b19ca360bfc1610d9785335ad599e9
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-09 13:25:02 -0700

    flaten arr one more solution

--- array/app.js
+++ array/app.js
@@ -966,3 +966,17 @@ function flatten(arr) {
     return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
   }, []);
 }
+
+// or
+
+function flatten(oldArr) {
+  var newArr = []
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]))
+    } else {
+      newArr.push(oldArr[i])
+    }
+  }
+  return newArr;
+}
