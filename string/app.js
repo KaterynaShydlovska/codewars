f569a72b0edddd22f56898047d4fd23636814f11
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-09 20:50:57 -0700

    Backspace String Compare

--- string/app.js
+++ string/app.js
@@ -333,3 +333,48 @@ function findLongestSubstring(str) {
   }
   return longest;
 }
+
+
+
+// Challenge 9
+
+// Backspace String Compare
+
+// Given two strings S and T, return if they are equal when both are typed into empty text editors.# means a backspace character.
+
+//   Example 1:
+
+// Input: S = "ab#c", T = "ad#c"
+// Output: true
+// Explanation: Both S and T become "ac".
+//   Example 2:
+
+// Input: S = "ab##", T = "c#d#"
+// Output: true
+// Explanation: Both S and T become "".
+//   Example 3:
+
+// Input: S = "a##c", T = "#a#c"
+// Output: true
+// Explanation: Both S and T become "c".
+//   Example 4:
+
+// Input: S = "a#c", T = "b"
+// Output: false
+// Explanation: S becomes "c" while T becomes "b".
+
+
+var backspaceCompare = function (S, T) {
+  const build = (S) => {
+    let st = [];
+    for (let i = 0, len = S.length; i < len; ++i) {
+      if (S[i] == '#') {
+        st.pop();
+      } else {
+        st.push(S[i]);
+      }
+    }
+    return st.join('');
+  }
+  return build(S) === build(T);
+};
