99fdffd277f1d7e4c25045f1d8fdfed070cfadee
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-11 21:56:35 -0700

    array reverse

--- array/app.js
+++ array/app.js
@@ -980,3 +980,18 @@ function flatten(oldArr) {
   }
   return newArr;
 }
+
+
+// Challenge 30
+
+// Write a function reverse which reverses a list(or in clojure's case, any list-like data structure)
+
+//   (the dedicated builtin(s) functionalities are deactivated)
+
+reverse = function (array) {
+  var newArray = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
