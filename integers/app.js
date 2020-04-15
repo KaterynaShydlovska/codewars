da67f15e717cce965e53397cc83e2398a6f2f7ba
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-14 18:05:34 -0700

    sum of division

--- integers/app.js
+++ integers/app.js
@@ -602,3 +602,22 @@ function factorial(x) {
   return x * factorial(x - 1);
 }
 
+
+// Challenge 22
+// Given a positive integer n, calculate the following sum:
+
+// n + n / 2 + n / 4 + n / 8 + ....
+// All elements of the sum are the results of integer division.
+
+//   Example
+// 25  => 25 + 12 + 6 + 3 + 1 = 47
+
+function halvingSum(n) {
+  var sum = 0;
+  while (n > 0) {
+    sum += n;
+    n = Math.floor(n / 2);
+  }
+  return sum;
+}
+
