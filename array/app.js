bd3046265d5eac1fe6492e4f84f844a7b547d120
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-17 19:12:18 -0700

    sum of 2 arr

--- array/app.js
+++ array/app.js
@@ -1109,3 +1109,19 @@ function swap(arr, i, j) {
   arr[j] = temp;
 }
 
+
+// Challenge 36
+
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+//   P.S.Each array includes only integer numbers.Output is a number too.
+
+
+function arrayPlusArray(arr1, arr2) {
+  let sum = 0;
+  let newAr = arr1.concat(arr2);
+  for (let i = 0; i < newAr.length; i++) {
+    sum += newAr[i]
+  }
+  return sum //something went wrong
+}
