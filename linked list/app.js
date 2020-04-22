9d7ce91762de63aac33deaf9d2c60939e95cff67
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-21 21:00:49 -0700

    concat 2 linked list

--- linked list/app.js
+++ linked list/app.js
@@ -502,6 +502,35 @@ function stringify(list) {
 }
 
 
+// Linked Lists - Append
+
+// Write an Append() function which appends one linked list to another.The head of the resulting list should be returned.
+
+// var listA = 1 -> 2 -> 3 -> null
+// var listB = 4 -> 5 -> 6 -> null
+// append(listA, listB) === 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+// If both listA and listB are null / NULL / None / nil, return null / NULL / None / nil.If one list is null / NULL / None / nil and the other is not, simply return the non - null / NULL / None / nil list.
+
+function Node(data) {
+
+  this.data = data;
+  this.next = null;
+}
+
+function append(listA, listB) {
+  // console.log(listA, listB)
+  if (!listA && !listB) return null;
+  if (!listA) return listB;
+  if (!listB) return listA;
+  let temp = listA
+  while (temp.next) {
+    temp = temp.next;
+  }
+  temp.next = listB;
+  return listA;
+}
+
+
 
 
 
