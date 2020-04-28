8ed6a4fdcb6f1acf989be4b2f7a4496dca7c67bd
katerynashydlovska <shydlovskayaka@gmail.com>
2020-04-28 16:26:20 -0700

    DoublyLinked List push

--- linked list/app.js
+++ linked list/app.js
@@ -716,6 +716,41 @@ function sortedInsert(head, data) {
   list.push("Harry")
   list.push("Ron")
   list.push("Hermione")
+  
+
+//  Doubly Linked List my implementation
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(val) {
+    this.val = val
+    this.next = null;
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
 
 
 
