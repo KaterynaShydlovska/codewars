47ad8b06d62ad8ff84108f9f0be46ce1bcae39b0
katerynashydlovska <shydlovskayaka@gmail.com>
2020-05-01 11:52:20 -0700

    Total amount of points

--- array/app.js
+++ array/app.js
@@ -1280,3 +1280,36 @@ var decompressRLElist = function (nums) {
   return res;
 
 };
+
+// Challenge 41
+
+// Our football team finished the championship.The result of each match look like "x:y".Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship.Rules for counting points for each match:
+
+//   if x > y - 3 points
+// if x < y - 0 point
+// if x = y - 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  let count = 0;
+  for (let i = 0; i < games.length; i++) {
+    console.log(games[i][0], games[i][2])
+    if (games[i][0] > games[i][2]) {
+      count += 3;
+    } else if (games[i][0] < games[i][2]) {
+      count += 0;
+    } else {
+      count += 1;
+    }
+
+  }
+  return count;
+}
