5c48393b75745a7e9691009ed1c05b4d87cb8b6b
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-06-12 17:05:18 -0700

    n-th power

--- array/app.js
+++ array/app.js
@@ -1442,3 +1442,26 @@ var searchInsert = function(nums, target) {
   return nums.length;
   
 };
+
+// Challenge 45
+
+// You are provided with array of positive non-zero ints and int n representing n-th power (n >= 2).
+
+// For the given array, calculate the sum of each value to the n-th power. Then subtract the sum of the original array.
+
+// Example 1: Input: {1, 2, 3}, 3 --> (1 ^ 3 + 2 ^ 3 + 3 ^ 3 ) - (1 + 2 + 3) --> 36 - 6 --> Output: 30
+
+// Example 2: Input: {1, 2}, 5 --> (1 ^ 5 + 2 ^ 5) - (1 + 2) --> 33 - 3 --> Output: 30
+
+function modifiedSum(a, n) {
+
+  //   let out = 0;
+    let sum =0;
+    let nth=0;
+    for(let i =0; i< a.length; i++){
+    sum += a[i];
+    nth+= Math.pow(a[i], n)
+    }
+    console.log(sum, nth)
+    return nth - sum;
+  }
