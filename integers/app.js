2aa6a8ae7f70972d89cb01b9432984781f4de553
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-07-29 13:10:28 -0700

    int

--- integers/app.js
+++ integers/app.js
@@ -792,3 +792,19 @@ function findMultiples(int,limit){
     
   return result
 }
+
+
+ // Challenge 29
+
+// An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+// arrow
+
+// a and b are integers and > 0
+
+// Write a function which returns the area of the arrow.
+
+function arrowArea(a,b) {
+  // your code
+  return a * b / 4;
+}
