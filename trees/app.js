7840f8cf972c5d3b934d036ed24d577e3af0e355
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-11-13 15:59:15 -0600

    bst sum

--- trees/app.js
+++ trees/app.js
@@ -408,4 +408,31 @@ tree.DFSInOrder();
 
 
   
+// Challenge 4
 
+// Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+// Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+// Output: 32
+
+// Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+// Output: 23
+
+
+var rangeSumBST = function(root, low, high) {
+  let sum =0;
+  
+  return bst(root, low, high )
+};
+
+function bst(node, low, hight, sum=0){
+  
+  if(!node){
+    return 0;    
+  }
+  
+  if(node.val >= low && node.val <= hight){
+      sum = node.val;
+  }
+  return sum + bst(node.left, low, hight) + bst(node.right, low, hight)
+
+}
